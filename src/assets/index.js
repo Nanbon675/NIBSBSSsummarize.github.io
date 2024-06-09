4d86965739475295423c6e04fd208b04160ac2a4
Nanbon675 <167584843+Nanbon675@users.noreply.github.com>
2024-06-09 16:42:39 +0300

    Add files via upload

---
+++ src/assets/index.js
@@ -0,0 +1,13 @@
+import linkIcon from './link.svg'
+import loader from './loader.svg'
+import copy from './copy.svg'
+import av from './av.png'
+import tick from './tick.svg'
+
+export {
+    linkIcon,
+    loader,
+    copy,
+    av,
+    tick
+}
